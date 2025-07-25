0284934b51adaf623cf53f3302361bc3c599768b
draghiciioan <75200645+draghiciioan@users.noreply.github.com>
2025-07-25 23:07:38 +0300

    Adăugat fișiere de configurare pentru proiect și actualizat submodul bee_web_service

--- src/pages/DashboardHome.tsx
+++
@@ -1,7 +0,0 @@
-export default function DashboardHome() {
-  return (
-    <div className="text-center text-green-400 text-2xl">
-      🧠 Bun venit în zona de superputeri BeeConect!
-    </div>
-  )
-}
--- src/pages/Login.tsx
+++
@@ -1,7 +0,0 @@
-export default function Login() {
-  return (
-    <div className="text-center text-cyan-400 text-xl">
-      Autentificare în curs... 🛡️ (aici va fi formularul)
-    </div>
-  )
-}
