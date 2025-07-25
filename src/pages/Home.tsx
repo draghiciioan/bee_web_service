f1bb70822b15a6efe4ae284071908af99c5b08b3
draghiciioan <75200645+draghiciioan@users.noreply.github.com>
2025-07-25 23:08:53 +0300

    Adăugat componentele Home, Login și DashboardHome cu mesaje de întâmpinare

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-center text-yellow-400 text-3xl font-bold">
+      Bine ai venit la <span className="text-cyan-400">BeeConect</span> 🚀
+    </div>
+  )
+}
